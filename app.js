247339b28b1c32b17e8bf9a91fdd9fc8d609d58f
maya <maya.keeley@outlook.com>
2020-07-27 10:04:13 +0100

    testing out stuff

---
+++ app.js
@@ -0,0 +1,5 @@
+let stuff = require("./stuff");
+
+console.log(stuff.counter(["shaun", "crystal", "ryu"]));
+console.log(stuff.adder(3, 2));
+console.log(stuff.adder(stuff.pi, 5));
---
+++ stuff.js
@@ -0,0 +1,11 @@
+const counter = (arr) => {
+  return `There are ${arr.length} items in this array.`;
+};
+
+const adder = (a, b) => {
+  return `The sum of the two numbers is ${a + b}`;
+};
+
+const pi = 3.142;
+
+module.exports = { counter, adder, pi };
